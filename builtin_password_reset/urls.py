46576be21ea05479ad7a667bb39211de41305d4d
Albert O'Connor <amjoconn@gmail.com>
2014-10-16 16:54:36 -0400

    Explicit default from email.

--- builtin_password_reset/urls.py
+++ builtin_password_reset/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, url
-
+from django.conf import settings
 from django.contrib.auth import views
 
+from_email = settings.DEFAULT_FROM_EMAIL
+
 urlpatterns = patterns('',
-    url(r'^reset_password/$', views.password_reset, name='reset_password'),
+    url(r'^reset_password/$', views.password_reset, {'from_email': from_email }, name='reset_password'),
     url(r'^reset_password/submitted/$', views.password_reset_done, name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z]+)/(?P<token>.+)/$', views.password_reset_confirm, name='password_reset_confirm'),
     url(r'^reset/complete/$', views.password_reset_complete, name='password_reset_complete'),
